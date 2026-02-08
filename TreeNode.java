a0737e267cef70cf41880d893b1defe6346507c7
SidhuSamar-lab <sidhusamarvir22@gmail.com>
2026-02-08 19:14:26 +0530

    Day 14 of leet code

---
+++ TreeNode.java
@@ -0,0 +1,67 @@
+// 110. Balanced Binary Tree
+
+// Example 1:
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: true
+// Example 2:
+
+// Input: root = [1,2,2,3,3,null,null,4,4]
+// Output: false
+// Example 3:
+
+// Input: root = []
+// Output: true
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 5000].
+// -104 <= Node.val <= 104
+
+
+
+
+
+
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+ 
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+    
+    private int checkHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        
+        int leftHeight = checkHeight(node.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        
+        int rightHeight = checkHeight(node.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
