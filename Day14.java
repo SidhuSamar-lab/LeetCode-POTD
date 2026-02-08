9e03ee07b02842ac96074f43ed6b8a8be202cdf9
SidhuSamar-lab <sidhusamarvir22@gmail.com>
2026-02-08 19:17:16 +0530

    Day 14

--- Day14.java
+++ Day14.java
@@ -25,13 +25,13 @@
 
 
 
-public class TreeNode {
+public class Day14 {
     int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    Day14 left;
+    Day14 right;
+    Day14() {}
+    Day14(int val) { this.val = val; }
+    Day14(int val, Day14 left, Day14 right) {
         this.val = val;
         this.left = left;
         this.right = right;
@@ -39,11 +39,11 @@ public class TreeNode {
 }
  
 class Solution {
-    public boolean isBalanced(TreeNode root) {
+    public boolean isBalanced(Day14 root) {
         return checkHeight(root) != -1;
     }
     
-    private int checkHeight(TreeNode node) {
+    private int checkHeight(Day14 node) {
         if (node == null) {
             return 0;
         }
